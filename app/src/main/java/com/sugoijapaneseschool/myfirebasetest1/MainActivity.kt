ea11a6b5d2fdaf5587a8c9e7a331b6aaafeb2e86
kimtaeho <>
2021-02-05 16:18:20 +0900

    2.Firebase analytics가 있으니, google analytics 를 연계한 앱분석용 로그를 firebase에 남겨보자.

--- app/src/main/java/com/sugoijapaneseschool/myfirebasetest1/MainActivity.kt
+++ app/src/main/java/com/sugoijapaneseschool/myfirebasetest1/MainActivity.kt
@@ -2,10 +2,19 @@ package com.sugoijapaneseschool.myfirebasetest1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
+    private var mFirebaseAnalytics: FirebaseAnalytics? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bundle = Bundle()
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "MyAPp")
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        mFirebaseAnalytics?.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
     }
 }
